aeeb2c1217b6236626a867d9a463117ea827d080
jacky <kalokchong2418@gmail.com>
2024-09-16 19:38:04 +0800

    session

---
+++ middleware.ts
@@ -0,0 +1,51 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { decrypt } from "./lib/session/session";
+
+// Define the protected routes
+const protectedRoutes = [
+  "/dashboard", // Add other protected routes here
+  "/profile",
+  "/settings",
+];
+
+export const middleware = async (req: NextRequest) => {
+  const res = NextResponse.next();
+
+  //Check if route is protected
+  const currentPath = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(currentPath);
+
+  if (isProtectedRoute) {
+    //check for valid session
+    const cookie = cookies().get("session")?.value as any;
+    const session = (await decrypt(cookie)) as any;
+
+    // Redirect unauthed users
+    if (!session && !session?.userId) {
+      return NextResponse.redirect(new URL("/login", req.nextUrl));
+    }
+  }
+
+  // if user is not signed in and the current path is not /login redirect the user to /login
+  if (req.nextUrl.pathname !== "/login" && req.nextUrl.pathname !== "/signup") {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  //Render Route
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes) login only
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api/login|_next/static|images|favicon.ico).*)",
+  ],
+};
--- server/routers/user.ts
+++ server/routers/user.ts
@@ -5,19 +5,19 @@ import { z } from "zod";
 const prisma = new PrismaClient();
 
 export const userRouter = router({
-    getUsers: procedure.query(async () => {
-        const result = await prisma.user.findMany();
-        return result;
+  getUsers: procedure.query(async () => {
+    const result = await prisma.user.findMany();
+    return result;
+  }),
+  addUser: procedure
+    .input(z.object({ name: z.string(), race: z.string() }))
+    .mutation(async (opts) => {
+      const { input } = opts;
+      await prisma.user.create({
+        data: {
+          email: input.name,
+          password: input.race,
+        },
+      });
     }),
-    addUser: procedure
-        .input(z.object({ name: z.string(), race: z.string() }))
-        .mutation(async (opts) => {
-            const { input } = opts;
-            await prisma.user.create({
-                data: {
-                    email: input.name,
-                    race: input.race,
-                }
-            })
-        })
-})
+});
